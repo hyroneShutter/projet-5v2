8adaff517379c581b0086eeda5cd5366d6af8922
hyroneShutter <124780887+hyroneShutter@users.noreply.github.com>
2024-05-08 11:31:08 +0200

    Ajout de listener fleche gauche droite et initialisation des dots

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,80 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let index = 0
+let arr = undefined
+const leftArrowListener = document.querySelector('.arrow_left');
+const rightArrowListener = document.querySelector('.arrow_right');
+const dots = document.querySelector(".dots");
+const slideLength = slides.length;
+
+function dispIdx () {
+  if (arr === 'l'){
+    console.log('index',index, 'click left --')
+  } else {
+    console.log('index',index, 'click right ++')
+  }
+}
+console.log('index', index);
+
+
+
+
+
+
+function listenerArrowLeft () {
+  leftArrowListener.addEventListener("click", () => {
+    arr = 'l'
+    index--
+    if (index == -1) {
+      index = slideLength -1
+    }
+
+
+    dispIdx();
+  });
+}
+listenerArrowLeft();
+
+
+
+
+function listenerArrowRight () {
+  rightArrowListener.addEventListener("click", () => {
+    arr = 0
+    index++
+    if (index > slideLength -1) {
+      index = 0
+    }
+    
+
+    dispIdx();
+  });
+}
+listenerArrowRight();
+
+
+/*---------------------------------------------------------------------------------------------*/
+
+
+
+
+
+
+function dotLine () {
+
+  for (let i = 0; i < slideLength; i++) {
+    const dot = document.createElement('div');
+    dot.classList.add('dot');
+    dots.appendChild(dot);
+    if (i == index) {
+      dot.classList.add("dot_selected")
+    }
+  }
+
+}
+dotLine()
+
+
+
